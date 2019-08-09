5b57bdbc498cb91f02533a77290d76d941c1d57c
unknown <cvlopes88@gmail.com>
2019-08-09 17:44:25 -0400

    sprint

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,85 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const containerCard = document.querySelector('.cards-container');
+
+    
+        
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+.then((response) => {
+  console.log(response);
+ newCard = response.data.map(i => x.name)
+
+// response.forEach(ite => {
+//     let newCar = component(ite);
+//     containerCard.appendChild(newCar);
+// })
+return {i: []}
+
+})
+
+
+
+//   
+//       let newArt = component(ite)
+//       containerCard.appendChild(newArt)
+//   })
+
+.catch( (err) => {
+    console.log(err)
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//////////////////////////////////
+
+function component(items){
+
+    const newCard = document.createElement('div');
+    newCard.classList.add('card');
+
+const newHead = document.createElement('div');
+newHead.classList.add('headline');
+newHead.textContent = items.headline;
+
+const newAuthor = document.createElement('div');
+newAuthor.classList.add('author');
+newAuthor.textContent = items.authorName;
+
+
+const newImg = document.createElement('img');
+newImg.classList.add('img-container');
+// newImg.src = items.authorPhoto;
+
+
+const newName = document.createElement('span');
+newName.textContent = items.authorName;
+
+newCard.appendChild(newImg);
+newCard.appendChild(newName);
+newCard.appendChild(newHead);
+newCard.appendChild(newAuthor);
+
+
+
+return newCard;
+
+}
--- components/Header/index.js
+++ components/Header/index.js
@@ -7,6 +7,40 @@
 //    <h1>Lambda Times</h1>
 //    <span class="temp">98°</span>
 //  </div >
-// And add it to the DOM in the .headerContainer component
+// And add it to the DOM in the .header-Container component
 
-function Header() {}
+
+
+
+
+
+
+const headContainer = document.querySelector('.header-container');
+
+headContainer.appendChild(Header());
+
+function Header(obj) {
+
+const headTop = document.createElement('div');
+const date1 = document.createElement('span');
+const title1 = document.createElement('h1');
+const temp1 = document.createElement('span');
+
+headTop.appendChild(date1);
+headTop.appendChild(title1);
+headTop.appendChild(temp1);
+
+headTop.classList.add('header');
+date1.classList.add('date');
+temp1.classList.add('temp');
+
+
+date1.textContent = 'August 09, 2019';
+title1.textContent = 'Lambda Times';
+temp1.textContent = '98';
+
+
+
+
+return headTop;
+}
